70dd39643ef9bbc06c69c299c0adf9590aa913c2
Katie Williams <kawilliams8@gmail.com>
2019-09-26 08:43:03 -0600

    Add keyframe pulsing-heart animation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Basic CSS Transition</h1>
-      <button href="#" className="button">Hover!</button>
+      <h2>Hover-based Color CSS Transition</h2>
+      <button href="#" className="button" id="colorshift">
+        Hover!
+      </button>
+      <h2>Keyframe-based Transformation</h2>
+      <button href="#" className="button" id="pulseanimation">
+        Hover!
+      </button>
     </div>
   );
 }
